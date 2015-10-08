f4f55b5271b2913d689b869b483b157907121237
Niklas Sombert <niklas@ytvwld.de>
2015-10-08 19:12:57 +0200

    Weniger kompliziert ist besser.

--- Beispiele/dateien.py
+++ Beispiele/dateien.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-with open("loremipsum.txt", "r") as lorem_ipsum:
-	text = lorem_ipsum.read()
+lorem_ipsum = open("loremipsum.txt", "r")
+text = lorem_ipsum.read()
 orig = text
 text = text.upper()
 if "U" in text:
 	text = text.replace("U", "V")
 print(text)
 print(orig)
-with open("loremipsvm.txt", "w") as lorem_ipsvm:
-	lorem_ipsvm.write(text)
+lorem_ipsvm = open("loremipsvm.txt", "w")
+lorem_ipsvm.write(text)

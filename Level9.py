fcdf1bfa9753ba40a565e16bcc826a6c7a501bd6
Niklas Sombert <niklas@ytvwld.de>
2016-05-03 20:25:49 +0200

    Level 9: die Doku verlinken statt Terminal-Output zu copy-pasten
    Was @bison-- (https://twitter.com/bison_42/status/726481835228798977)
    und @dodonator (https://twitter.com/dodo_CD/status/727086613164089344) sagten.

--- Level9.py
+++ Level9.py
@@ -18,47 +18,10 @@ import sys
 
 """
 Am Anfang muss ein QApplication-Objekt erstellt werden.
-sys.argv wird durchgeschleift, damit Qt u.a. auf folgende Parameter reagieren kann:
+sys.argv wird durchgeschleift, damit Qt auf einige Parameter reagieren kann
+(u.a. diese: https://doc.qt.io/qt-5/qapplication.html#QApplication).
 
-$ konsole --help-qt
-> Qt options:
->  --display <displayname>   Use the X-server display 'displayname'
->  --session <sessionId>     Restore the application for the given 'sessionId'
->  --cmap                    Causes the application to install a private color
->                            map on an 8-bit display
->  --ncols <count>           Limits the number of colors allocated in the color
->                            cube on an 8-bit display, if the application is
->                            using the QApplication::ManyColor color
->                            specification
->  --nograb                  tells Qt to never grab the mouse or the keyboard
->  --dograb                  running under a debugger can cause an implicit
->                            -nograb, use -dograb to override
->  --sync                    switches to synchronous mode for debugging
->  --fn, --font <fontname>   defines the application font
->  --bg, --background <color> sets the default background color and an
->                            application palette (light and dark shades are
->                            calculated)
->  --fg, --foreground <color> sets the default foreground color
->  --btn, --button <color>   sets the default button color
->  --name <name>             sets the application name
->  --title <title>           sets the application title (caption)
->  --testability             load the testability framework
->  --visual TrueColor        forces the application to use a TrueColor visual on
->                            an 8-bit display
->  --inputstyle <inputstyle> sets XIM (X Input Method) input style. Possible
->                            values are onthespot, overthespot, offthespot and
->                            root
->  --im <XIM server>         set XIM server
->  --noxim                   disable XIM
->  --reverse                 mirrors the whole layout of widgets
->  --stylesheet <file.qss>   applies the Qt stylesheet to the application widgets
->  --graphicssystem <system> use a different graphics system instead of the default one, options are raster and opengl (experimental)
->  --qmljsdebugger <port>    QML JS debugger information. Application must be
->                            built with -DQT_DECLARATIVE_DEBUG for the debugger to be
->                            enabled
->  --platform <platform>     The windowing system platform (e.g. xcb or wayland)
-
-(Ja, wir bekommen die Wayland-Kompatibilität geschenkt.)
+(Die Wayland-Kompatibilität mittels "--platform wayland" bekommen wir geschenkt.)
 """
 
 app = QApplication(sys.argv)

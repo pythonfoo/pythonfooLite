f2b34ac2c1c9e908003d0b276c5b11109f5bc7de
Olaf Gladis <olaf.gladis@trivago.com>
2016-12-22 11:47:44 +0100

    show else block in for loop

--- Beispiele/pwd-schleifen.py
+++ Beispiele/pwd-schleifen.py
@@ -3,14 +3,21 @@
 from getpass import getpass
 
 PWD = "123456"
+N_VERSUCHE = 3
 
-for x in range(3):
-	eingabe = getpass()
-	
-	if eingabe == PWD:
-		print("Richtig.")
-		break
-	elif eingabe in PWD:
-		print("Fast.")
-	else:
-		print("Falsch.")
+for x in range(N_VERSUCHE):
+    eingabe = getpass()
+
+    if eingabe == PWD:
+        print("Richtig.")
+        break
+    elif eingabe in PWD:
+        print("Fast.")
+    else:
+        print("Falsch.")
+else:
+    print("Passwort {} mal falsch eingegeben.".format(N_VERSUCHE))
+    import sys
+    sys.exit(1)
+
+print("...")

bc1c990f509bf40a5ced114da3241d5320a3b24c
Olaf Gladis <olaf.gladis@trivago.com>
2016-12-22 11:43:27 +0100

    use with statement to open files

--- Beispiele/dateien.py
+++ Beispiele/dateien.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
-lorem_ipsum = open("loremipsum.txt", "r")
-text = lorem_ipsum.read()
+with open("loremipsum.txt", "r") as lorem_ipsum:
+    text = lorem_ipsum.read()
+
 orig = text
 text = text.upper()
+
 if "U" in text:
-	text = text.replace("U", "V")
+    text = text.replace("U", "V")
+
 print(text)
 print(orig)
-lorem_ipsvm = open("loremipsvm.txt", "w")
-lorem_ipsvm.write(text)
+
+with open("loremipsvm.txt", "w") as lorem_ipsvm:
+    lorem_ipsvm.write(text)

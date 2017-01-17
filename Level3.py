a3c8d13f3bfb7d2485cc448ef56bdb5d2953ebe0
dodo <dodo@chaosdorf.de>
2017-01-17 08:19:58 +0100

    .pop(), .count(), .insert(), .remove(), .sort() hinzugefügt

--- Level3.py
+++ Level3.py
@@ -7,32 +7,74 @@
 
 # 1. Die Liste:
 
-# Eine Liste ist eine veränderbare und unsortierte
-# Menge von Elementen, welche alles mögliche sein
-# können. Unsortiert heißt in dem Fall, dass es eine Rolle
-# spielt, in welcher Reihenfolge die Elemente hinzugefügt werden.
+# Eine Liste ist eine beliebig lange Folge von beliebigen Objekten
 
 # Eine Liste wird mit Hilfe von eckigen Klammern definiert.
 liste = [0, "foo"]
 print(liste)
+
+
+# Mit list() lässt sich bspw. ein String in einen Liste verwandeln:
+print(list(String))
+
 
 # Auf ein Element in einer Liste wird über dessen Index
 # zugegriffen. Der Index ist die Stelle, an der das Element
 # steht.
 # Wichtig: Die Zählung des Index beginnt mit 0,
 # daher ist der Index des ersten Elements 0.
+# Liste: [0, "foo"]
+# Index:  0    1
+
 element = liste[0]
 print(element)
 
-# Mit len() kann man sich die Anzahl an Elementen einer
-# Liste ausgeben lassen:
+# Die Funktion liefert für viele Objekte die Länge zurück.
+# Bei einer Liste enspricht die Länge der Anzahl an Elementen.
 l = len(liste)
 print(l)
+
 
 # Die append()-Methode fügt einer Liste ein beliebiges
 # Element hinzu:
 liste.append("bar")
 print(liste)
+
+
+# Statt ein Objekt am Ende einer Liste anzufügen, ist es auch möglich,
+# es an einem Index einzusetzen. Dabei wird das Objekt vor dem Index
+# eingesetzt.
+liste.insert(0, "test")
+print(liste)
+
+
+# Die pop()-Methode löscht das Objekt an dem Index in der Liste.
+# Ist kein Index angegeben lösht pop() das letzte Element
+liste.pop()
+print(liste)
+
+
+# Ein Element kann aber nicht nur über den Index gelöscht werden, sondern
+# auch über das Objekt, es wird allerdings nur das erste Auftreten des
+# Objektes gelöscht. Dabei wird ein Fehler geworfen, falls das Objekt
+# nicht in der Liste vorhanden ist.
+liste2.remove(9)
+print(liste2)
+
+
+# Um festzustellen, wie oft ein Wert in einer Liste vorhanden ist kann
+# die count()-Methode verwendet werden.
+liste3 = list("aabbbcccc")
+print(liste3.count("a"))
+print(liste3.count("d"))
+
+
+# Eine Liste kann mit sort() sortiert werden:
+liste2 = [9,6,3,2,7]
+liste2.sort()
+print(liste2)
+
+
 
 # Der Datentyp <String> hat große Ähnlichkeit mit dem
 # Datentyp <Liste>. So funktioniert der Zugriff über den
@@ -48,9 +90,6 @@ print(String)
 # Dabei ist 'String += "XYZ"' eine Abkürzung für
 # String = String + "XYZ", was bedeutet, dass ein
 # String neu definiert und nicht verändert wird.
-
-# Mit list() lässt sich ein String in einen Liste verwandeln:
-print(list(String))
 
 
 # 2. Das Tuple:

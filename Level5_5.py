d1a174d03188f95ea193c43eff13950cdd4bd385
dodo <dodo@chaosdorf.de>
2017-03-07 19:06:23 +0100

    escaping und rawstrings

--- Level5_5.py
+++ Level5_5.py
@@ -1,4 +1,22 @@
-# Mehr Stringmethoden:
+# Character escaping:
+# escaping ist eine Möglichkeit Zeichen und Steuerzeichen (z.B. \n und \t) in einen
+# String einzubauen
+print("Tab:\t#")
+
+# Das Zeichen hinter einem \ wird entweder Steuerzeichen interpretiert oder ignoriert
+print("\aabc")
+
+# Folglich kann ein \ nicht trivial in einen String gepackt werden:
+# Nicht so:
+print("\0/")
+# Sondern so:
+print("\\0/")
+
+# Rawstrings können kein escaping:
+print(r"\n")
+print(r"\0/")
+
+
 # string.format()
 print("Das folgende Wort wird ersetzt: '{}' Der Rest nicht.".format("blargh"))
 # Auch:
@@ -6,11 +24,12 @@ print("Das folgende Wort wird ersetzt: '{0} und {1}' Der Rest nicht.".format("fo
 
 # String.split()
 s = "a;b;cd"
-s = s.split(";")
+l = s.split(";")
+print(repr(l))
 # l entspricht nun: ["a", "b", "cd"]
 # Geeignet zum Parsen von .csv Dateien zum Beispiel
 
-l = ";".join(s)
+s = ";".join(l)
 # l enspricht nun: 'a;b;cd'
 
 # Wiederholung: 

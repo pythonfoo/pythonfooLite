f8eeb02a3ea0c27211e717bbbecfee86cf8e86ab
Niklas Sombert <niklas@ytvwld.de>
2017-03-07 19:00:11 +0100

    Level 1 aufgespalten.

---
+++ Level_1/integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+x = 5
+z = -3
+basis = 2
+exponent = 5
+
+# Daten andere Typen in Integer umwandeln
+# Achtung: Dies kann fehlschlagen!
+y = int("6")
+
+# Operatoren auf Strings anwenden:
+# (für mehr Informationen siehe die Wiki-Seite zu Operatoren)
+summe = x + y
+produkt = x * z
+quotient = 9 / 3
+differenz = x - z
+potenz = pow(basis, exponent)  # Oder: basis ** exponent

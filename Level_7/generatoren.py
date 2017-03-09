ac3db0eddf48989b971cae5bceeadfe0159b5e6a
Niklas Sombert <niklas@ytvwld.de>
2017-03-09 21:59:43 +0100

    Level 7 aufgespalten.

--- Level_7/generatoren.py
+++ Level_7/generatoren.py
@@ -35,18 +35,3 @@
 Return:
     4
 4
-
-# Exceptions
->> > try:
-...     1 / 0
-...
-... except ZeroDivisionError as exc:
-... print(exc)
-...
-... finally:
-... print("Fertig.")
-...
-division by zero
-Fertig.
-
-# Nett ist auch "raise" innerhalb eines except-Blocks.

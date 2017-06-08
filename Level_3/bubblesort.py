0b9a2f2c7f225c0871029dd2a6aedbb60ae73780
dodo <dodo@chaosdorf.de>
2017-06-08 10:13:28 +0200

    Vorlage für die Bubblesortaufgabe

---
+++ Level_3/bubblesort.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import random
+
+def get_random_list(n):
+    result = list(range(n))
+    random.shuffle(result)
+    return result
+    
+n = int(input("Länge der Liste: ")
+unsortet_list = get_random_list(n)
+
+# Bitte die Zeilen 1-12 unverändert lassen

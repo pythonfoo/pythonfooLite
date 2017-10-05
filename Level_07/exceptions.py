55c28c36891bfe467e2f74328505ddb92f778ee8
dodo <dodo@chaosdorf.de>
2017-10-05 21:33:47 +0200

    Interpretermist entfernt

--- Level_07/exceptions.py
+++ Level_07/exceptions.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 # Exceptions
->> > try:
-...     1 / 0
-...
-... except ZeroDivisionError as exc:
-... print(exc)
-...
-... finally:
-... print("Fertig.")
-...
-division by zero
-Fertig.
+try:
+    1 / 0
+
+except ZeroDivisionError as exc:
+    print(exc)
+
+finally:
+    print("Fertig.")
+
+# OUT:
+# division by zero
+# Fertig
 
 # Nett ist auch "raise" innerhalb eines except-Blocks.
+
+try:
+    x = input()
+    x = int(x)
+except ValueError:
+    print(x, " kann nicht als Integer benutzt werden.")
+    raise
+
+# So wird der Fehler bearbeitet, aber der Fehler bleibt nicht unentdeckt
+    

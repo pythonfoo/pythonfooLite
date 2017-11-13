8d740722e8b50fea11533248294576aef638c1a7
dodo <dodo@chaosdorf.de>
2017-11-13 10:56:06 +0100

    Beispiellösung für die zweite Aufgabe

---
+++ Level_01/Aufgaben/print_string.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Dies ist eine Beispielslösung für die Aufgabe 2 aus Level 1:
+
+# 1. Schreibe ein Programm, das den String "foo" ausgibt
+print("foo") # Sehr ähnlich dem "hello world" Code
+
+
+# 2. Ändere das Programm so ab, dass es den String "foo" 5 mal ausgibt:
+print("foo")
+print("foo")
+print("foo")
+print("foo")
+print("foo")
+
+
+# 3. Ändere das Programm so ab, dass der String 5 mal in der selben Zeile ausgegeben wird.
+print(5*"foo")
+
+
+# 4. Ändere das Programm so ab, dass der Benutzer eingeben kann welcher String 5 mal in der
+#    selben Zeile ausgegeben werden soll.
+string = input("Welcher String soll ausgegeben werden? ")
+print(5*string)
+
+
+# 5. Ändere das Programm so ab, dass der Benutzer angeben kann, wie oft der angegebene String
+#    ausgegeben werden soll.
+string = input("Welcher String soll ausgegeben werden? ")
+wiederholung = input("Wie oft soll '" + string + "' ausgegeben werden? ")
+# Hier wird in dem Aufruf von input() ein String zusammen gebaut
+
+# Wie in Aufgabe 1 muss auch hier der String in einen Integer umgewandelt werden
+wiederholung = int(wiederholung)
+
+print(wiederholung*string)

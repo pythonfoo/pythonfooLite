6911896516e92afd84a477f7f919e816c68e53a7
dodo <dodo@chaosdorf.de>
2018-02-10 11:19:53 +0100

    Eine fehlerhafte Datei zum korigieren

---
+++ Level_01/Aufgaben/buggy.py
@@ -0,0 +1,15 @@
+# In dieses Programm sind verschiedene Fehler eingebaut,
+# die Aufgabe besteht darin die Fehler zu finden und zu beheben.
+
+a = inpu("Bitte eine Zahl eingeben: ")
+b = input("Bitte eine weitere Zahl eingeben: )
+
+summe = a + b
+differenz = abs(a - b)
+produkt = a * b
+quotient = a / b
+
+ print("Summe: " + summe)
+print("Differenz: " + diferenz)
+print("Produkt: " + produkt)
+print("Quotient: " + Quotient)

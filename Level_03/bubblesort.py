5749f7403c4acff37afe7a4b3c6ec32418a9c421
Niklas Sombert <niklas@ytvwld.de>
2018-03-08 20:17:56 +0100

    BubbleSort: Fehlende Klammer ergänzt.

--- Level_03/bubblesort.py
+++ Level_03/bubblesort.py
@@ -6,7 +6,7 @@ def get_random_list(n):
     random.shuffle(result)
     return result
     
-n = int(input("Länge der Liste: ")
+n = int(input("Länge der Liste: "))
 unsortet_list = get_random_list(n)
 
 # Bitte die Zeilen 1-12 unverändert lassen

8f09e0f39c37e44f861f11cd3c07440320911326
Niklas Sombert <niklas@ytvwld.de>
2018-06-14 20:29:51 +0200

    Level 05: ggT: negative Zahlen korrekt

--- Level_05/ggT.py
+++ Level_05/ggT.py
@@ -7,16 +7,16 @@ Dieses Programm implementiert den euklidischen Algorithmus zur Bestimmung des gr
 
 # Berechnung
 def ggT(a: int, b: int) -> int:
+    # Beide Zahlen sollten positiv sein.
+    # Wir nehmen einfach den Betrag.
+    a = abs(a)
+    b = abs(b)
+    
     # a soll größer sein als b.
     # Falls das nicht bereits der Fall ist,
     # tauschen wir die beiden einfach.
     if b > a:
         return ggT(b, a)
-    
-    # Beide Zahlen sollten positiv sein.
-    # Wir nehmen einfach den Betrag.
-    a = abs(a)
-    b = abs(b)
     
     # Wenn b Null ist, sind wir fertig und a ist der ggT.
     # Ansonsten müssen wir (nochmal) rechnen.

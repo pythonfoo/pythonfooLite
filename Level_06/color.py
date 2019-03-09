28cc8527b3ceacda02ddc59a153b0e9e0f503cc3
dodo <dodo@chaosdorf.de>
2019-03-09 21:42:10 +0100

    kleinere Änderung

--- Level_06/color.py
+++ Level_06/color.py
@@ -3,6 +3,8 @@
 # huepy muss vorher installiert werden.
 import huepy
 
+print(huepy.bold(huepy.red("red and bold")))
+
 print(huepy.run("Starte..."))
 print(huepy.info("Info!!"))
 print(huepy.bad("Schlecht!"))
@@ -14,3 +16,4 @@ from tqdm import tqdm
 
 for x in tqdm(range(5000000), desc=huepy.run("Dinge")):
     pass
+

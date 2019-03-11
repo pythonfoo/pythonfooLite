0d12ab1d9aab383e5750058093a034ba5cb3871f
Niklas Sombert <niklas@ytvwld.de>
2019-03-11 20:59:35 +0100

    Sets: initial

---
+++ Level_03/sets.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Sets sind Mengen (im mathematischen Sinne), also:
+# * Elemente können maximal einfach vorkommen
+# * Es gibt keine Reihenfolge.
+
+s = {1, 2, 2, 2, 2, 2, 3, 'foo'}
+s.update({5})
+s
+set('foo')
+type({})

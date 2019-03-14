3eee8e1308357d6bfdf44d08f98a3d62b6031f76
dodo <dodo@chaosdorf.de>
2019-03-14 19:15:50 +0100

    zip, filter, lambda hinzugefügt

--- Level_08/map.py
+++ Level_08/map.py
@@ -16,3 +16,28 @@ for i in l:
 result = list(map(add_2, l))
 
     
+# oder:
+result = [add_2(x) for x in l]
+
+# filter
+
+def even(n):
+    return n % 2 == 0
+
+r = range(100)
+even_numbers = list(filter(even, r))
+
+even_numbers = [x for x in r if even(x)]
+print(even_numbers)
+
+# zip
+iter1 = range(10)
+iter2 = range(-10,0)
+print(list(zip(iter1, iter2)))
+
+# lambda
+print(list(filter(lambda x: x % 2 == 0, r)))
+
+print(all((even(x) for x in even_numbers)))
+
+# für mehr Spaß mit Generatoren: https://docs.python.org/3/library/itertools.html

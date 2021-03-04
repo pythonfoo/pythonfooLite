f334ac16308bc971c5f476f996134b562d12f68b
dodo <dodo@chaosdorf.de>
2021-03-04 18:57:24 +0100

    beispielslösung bubblesort

---
+++ Level_03/Aufgaben/bubblesort.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import random
+
+def get_random_list(n: int) -> list:
+    result = list(range(n))
+    random.shuffle(result)
+    return result
+    
+n = int(input("Länge der Liste: "))
+unsorted_list = get_random_list(n)
+
+# Bitte die Zeilen 1-12 unverändert lassen
+
+def bubblesort(unsorted_list):
+    changed = True
+    while changed:
+        changed = False
+        for i in range(len(unsorted_list)-1):
+            if unsorted_list[i] > unsorted_list[i+1]:
+                unsorted_list[i], unsorted_list[i+1] = unsorted_list[i+1], unsorted_list[i]
+                changed = True
+
+print(unsorted_list)
+bubblesort(unsorted_list)
+print(unsorted_list)

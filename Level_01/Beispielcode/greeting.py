bc83a3c23a6239e07a187e3e51aad7bd0961d79a
dodonator <dodo@chaosdorf.de>
2022-10-06 21:04:57 +0200

    Ein einfaches Programm, welches eine Grußbotschaft ausgibt.

---
+++ Level_01/Beispielcode/greeting.py
@@ -0,0 +1,7 @@
+"""
+Das Programm soll den Benutzer mit einer Grußnachricht begrüßen.
+"""
+
+username = input("Please input your name: ")  # Eingabe des Nutzernamens
+greeting = "Hello " + username  # Verarbeitung des Nutzernamens
+print(greeting)  # Ausgabe der Grußbotschaft
